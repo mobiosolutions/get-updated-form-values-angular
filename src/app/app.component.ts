527ceee26e430b4a5b8959c5d6d7ebc2fb67fea2
Ankit Gupta <ankitgupta@Ankits-MacBook-Air.local>
2020-03-04 12:08:33 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'get-updated-form-values-angular';
+
+  userForm: any;
+  compareValue?: any
+  updatedValues = {};
+  constructor(private formBuilder: FormBuilder) {
+    this.compareValue = {
+      name: 'yaxa',
+      email: 'yaxa@gmail.com',
+      profile: 'hello yaxa'
+    };
+    this.userForm = this.formBuilder.group({
+      name: [this.compareValue.name, Validators.required],
+      email: [this.compareValue.email, Validators.required],
+      profile: [this.compareValue.profile, [Validators.required, Validators.minLength(10)]]
+    });
+  }
+  save() {
+    console.log("save called");
+    this.updatedValues = this.getUpdatedValues(this.userForm, this.compareValue);
+    //return updatedValues; 
+    console.log("updatedValues",this.updatedValues);
+  }
+
+  getUpdatedValues = (form: any, compareValue?: any) => {
+    let updatedValues = {};
+    Object.keys(form.controls)
+      .forEach((key) => {
+        const currentControl = form.controls[key];
+        if (currentControl.dirty) {
+          if (currentControl.controls) {
+            let controls = this.getUpdatedValues(currentControl, compareValue[key]);
+            if (Object.entries(controls).length != 0) {
+              updatedValues[key] = controls;
+            }
+          } else if (! compareValue) {
+            updatedValues[key] = currentControl.value;
+          }
+          else if (compareValue[key] != currentControl.value){
+            updatedValues[key] = currentControl.value;
+          }
+        }
+    });
+    return updatedValues;
 }
+}
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule }   from '@angular/forms';
+
+import { NgxJsonViewerModule } from 'ngx-json-viewer';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxJsonViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
